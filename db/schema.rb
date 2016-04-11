b327cfb9fb81c9005198dade0417aff223c55ed7
Gustavo Pchek <ghpk88@gmail.com>
2016-04-11 20:02:44 -0300

    Projetos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408223150) do
+ActiveRecord::Schema.define(version: 20160411224819) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20160408223150) do
   create_table "projects", force: :cascade do |t|
     t.string   "name",              null: false
     t.string   "description"
-    t.integer  "token"
+    t.string   "token",             null: false
     t.integer  "parent_project_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
