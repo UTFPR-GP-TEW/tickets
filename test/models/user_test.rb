aabf027a32338e4027a09447ca970f52dad474f4
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2016-04-25 19:00:32 -0300

    Comentado arquivos de testes.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
+# require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
+# class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-end
+# end
