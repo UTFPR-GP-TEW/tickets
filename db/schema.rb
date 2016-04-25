73449a1268d97fe3713af531b2928af8d05b2278
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2016-04-25 20:29:59 -0300

    fix conflict

--- db/schema.rb
+++ db/schema.rb
@@ -52,4 +52,5 @@ ActiveRecord::Schema.define(version: 20160418224525) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
 end
