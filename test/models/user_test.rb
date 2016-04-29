a505553c89ba1da9d6b0ae64935abbb0d267895e
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2016-04-29 19:28:47 -0300

    Criando teste de inserção, Modelo User.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-# end
+class UserTest < ActiveSupport::TestCase
+	test "should create user" do
+		assert_difference('User.count', 1) do
+			User.create :email=> "name@example.com", :password => "12345678", :password_confirmation => "12345678"
+		end
+	end
+end
