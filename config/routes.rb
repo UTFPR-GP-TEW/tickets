48817187b3c1993917df879de71b32ad4bfcbba2
andre.gomes <work.andregomes@gmail.com>
2016-05-02 20:28:08 -0300

    Alteração para namespace admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :tickets
 
-  resources :tickets
+    resources :projects
 
-  resources :projects
+    root 'admin#index'
+  end
 
   get 'home/index'
 
