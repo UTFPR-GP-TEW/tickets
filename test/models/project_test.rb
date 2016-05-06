f41674bcc3f07219eaa8111f32001bfa578d5043
Gustavo Pchek <ghpk88@gmail.com>
2016-05-06 19:43:07 -0300

    Adding user ref to projects

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -7,15 +7,15 @@ class ProjectTest < ActiveSupport::TestCase
 
   test "should not project create without name" do
     assert_difference('Project.count', 0) do
-      Project.create description: 'None', token: SecureRandom.uuid, parent_project_id: nil
+      Project.create description: 'None', token: SecureRandom.uuid, parent_project_id: nil, user_id: 1
     end
   end
 
   test "should not duplicate token" do
     token = SecureRandom.uuid
     assert_difference('Project.count', 1) do
-      Project.create name: 'None1', description: 'None1', token: token, parent_project_id: nil
-      Project.create name: 'None2', description: 'None2', token: token, parent_project_id: nil
+      Project.create name: 'None1', description: 'None1', token: token, parent_project_id: nil, user_id: 1
+      Project.create name: 'None2', description: 'None2', token: token, parent_project_id: nil, user_id: 1
     end
   end
 end
