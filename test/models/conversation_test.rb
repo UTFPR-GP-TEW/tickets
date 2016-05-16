7cede34f5df90c084da82d6408ab5052dfe90920
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2016-05-16 20:06:19 -0300

    Defindo identação como '2 espaços'.

--- test/models/conversation_test.rb
+++ test/models/conversation_test.rb
@@ -2,17 +2,18 @@ require 'test_helper'
 
 class ConversationTest < ActiveSupport::TestCase
 
-	test "should create conversation" do
-		@ticket = tickets(:one)
-		assert_difference('Conversation.count', 1) do
-			Conversation.create content: 'test', ticket: @ticket
-		end
-	end
+  test "should create conversation" do
+    @ticket = tickets(:one)
+    
+    assert_difference('Conversation.count', 1) do
+      Conversation.create content: 'test', ticket: @ticket
+    end
+  end
 
-	test "should not commennt create without ticket" do
-		assert_difference('Conversation.count', 0) do
-			Conversation.create content: 'test'
-		end
-	end
+  test "should not commennt create without ticket" do
+    assert_difference('Conversation.count', 0) do
+      Conversation.create content: 'test'
+    end
+  end
 
 end
