b6ac8f07d370fd0c977e097e3852635be5012ce5
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2016-05-16 19:40:49 -0300

    Criando testes básicos para o modelo Conversation.

--- test/models/conversation_test.rb
+++ test/models/conversation_test.rb
@@ -1,10 +1,18 @@
 require 'test_helper'
 
 class ConversationTest < ActiveSupport::TestCase
-  def setup
-    @conversation = conversation(:one)
-  end
 
-  # TODO
+	test "should create conversation" do
+		@ticket = tickets(:one)
+		assert_difference('Conversation.count', 1) do
+			Conversation.create content: 'test', ticket: @ticket
+		end
+	end
+
+	test "should not commennt create without ticket" do
+		assert_difference('Conversation.count', 0) do
+			Conversation.create content: 'test'
+		end
+	end
 
 end
