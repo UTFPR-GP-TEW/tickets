407f3552feeb12db7999189d0e42fd6887ae05ea
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2016-05-20 19:47:01 -0300

    Criando testes funcionais.

--- test/controllers/conversations_controller_test.rb
+++ test/controllers/conversations_controller_test.rb
@@ -4,11 +4,22 @@ class ConversationsControllerTest < ActionController::TestCase
   setup do
     @conversation = conversations(:one)
   end
-  # TODO
 
   test "should destroy conversation" do
     assert_difference('Conversation.count', -1) do
       delete :destroy, id: @conversation
     end
+  end
+
+  test "should add conversation" do
+    @ticket = tickets :one
+
+    assert_difference('Conversation.count', 1) do
+
+      # Simulate a POST response with the given HTTP parameters.
+      post(:create, conversation: { content: 'Love Hina', ticket_id: @ticket }, format: :xwr)
+    end
+
+    # assert_response :sons
   end
 end
