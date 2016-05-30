b584d358d6e6e2d74143a4ce9b724bf2ee142cc1
Fabio L. Janiszevski <contato@fabiosammy.com>
2016-05-30 19:16:38 -0300

    Updated simplecov to rails format

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 if ENV['SIMPLECOV'] == 'simplecov'
   require 'simplecov'
-  SimpleCov.start
+  SimpleCov.start 'rails'
 else
   require 'coveralls'
   Coveralls.wear!('rails')
